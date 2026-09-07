dcd2fdb20cb25fa93d19a571599997cb4f3409c7
Fabian Caro <carofabian01@hotmail.com>
2026-09-07 12:04:59 -0500

    modificación en settings y engine, agregación de dependencias necesarias para hacer deploy con pg

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str = "sqlite:///tasks.db"
+    debug: bool = False
 
     model_config = SettingsConfigDict(
         env_file=".env",
--- Backend/infrastructure/database/engine.py
+++ Backend/infrastructure/database/engine.py
@@ -1,8 +1,14 @@
 from core.settings import settings
 from sqlmodel import create_engine
 
+connect_args = {}
+
+if settings.database_url.startswith("sqlite"):
+    connect_args["check_same_thread"] = False
+
+
 engine = create_engine(
     settings.database_url,
     echo=True,
-    connect_args={"check_same_thread": False},
+    connect_args=connect_args,
 )
